62707ecf9035ab24552f310c49184ddb834c9828
wangzhijian.001 <wangzhijian.001@bytedance.com>
2021-03-22 23:47:24 +0800

    update

---
+++ deploy_github_pages.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#######################################################
+##  Desc: 提交blog到github仓库并部署到hexo菜园子     ##
+##  Date: 2021-03-22                                 ##
+#######################################################
+
+##########Step1:提交变更到Github仓库##############
+git add -A
+git commit -m "update"
+git push origin hexo
+
+##########Step2:部署内容到hexo站点################
+hexo clean
+hexo generate
+hexo deploy
